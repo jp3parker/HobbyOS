35e4ba5ab03e00e55e4ebebacbbdf0d9eece12db
jp3parker <jp3parker@gmail.com>
2026-08-15 02:21:04 -0500

    fixed overwriting bug and scroll down bug

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -63,9 +63,10 @@ void terminal_putchar(char c) {
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH || c == '\n') {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
+		if (++terminal_row == VGA_HEIGHT) {
 			terminal_scrolldown();
-      	--terminal_row;
+			--terminal_row;
+		}
 	}
 }
 
